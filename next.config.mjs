dea6ac72921304d41833a78ceafd6b3e088c1330
DragonZilla <asdrfc1254@gmail.com>
2026-04-18 13:54:05 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  basePath: "/Senvvio", // Tells Next.js to add this to the start of all /paths
   images: {
     unoptimized: true,
   },
